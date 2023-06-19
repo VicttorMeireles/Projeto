4befcdf63ad8e49b2c7eb0a038186c9ee02bff0c
VicttorMeireles <victormgueiros@gmail.com>
2023-06-19 19:49:41 -0300

    adc js a page ajustes e setup

--- script-2.js
+++ script-2.js
@@ -1,16 +1,37 @@
-const slcQG = document.querySelector("#QG")
-const url = "http://127.0.0.1:5500/Inspecaodepecas.html"
-const url2 = "https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwj83s-arM7_AhVslZUCHdjZBM4QPAgI"
+//NILES//
+const niles = document.querySelector("#niles")
+const url19 = "http://127.0.0.1:5500/5-ajuste-medidas-niles.html"
+const url21 = "http://127.0.0.1:5500/5-setup.html"
 
-function openOptions(){
+function openNiles(){
 
-    for(i = 0; i < slcQG.options.length; i++)
-    if (slcQG.value == "1" ){
-        window.location.href = url
+    for (i = 0; i < niles.options.length; i++)
+    if (niles.value == "19"){
+        window.location.href = url19
     }
-    else if (slcQG.value == "2"){
-        window.location.href = url2
+    else if ( niles.value == "21"){
+        window.location.href = url21
     }
 }
 
-slcQG.addEventListener("change", openOptions)
+niles.addEventListener("change", openNiles)
+
+
+//QUALITY GATE//
+
+const QG = document.querySelector("#QG")
+const url33 = "http://127.0.0.1:5500/8-Inspecaodepecas.html"
+const url34 = "http://127.0.0.1:5500/8-rugosidade.html"
+
+function openOptions(){
+
+    for (i = 0; i < QG.options.length; i++)
+    if (QG.value == "33" ){
+        window.location.href = url33
+    }
+    else if (QG.value == "34"){
+        window.location.href = url34
+    }
+}
+
+QG.addEventListener("change", openOptions)
