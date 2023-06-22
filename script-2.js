cf0e6a1bfcf815825713a5f66de2b28140bca170
VicttorMeireles <victormgueiros@gmail.com>
2023-06-22 20:05:09 -0300

    js link na pagina 4-start-maquina

--- script-2.js
+++ script-2.js
@@ -1,19 +1,36 @@
+//PITTLER//
+
+const pittler = document.querySelector("#pittler")
+const url13 = "http://127.0.0.1:5500/4-start-maquina.html"
+
+function openPittler() {
+
+    for (i = 0; pittler.options.length; i++)
+        if (pittler.value == "13") {
+            window.location.href = url13
+        }
+}
+
+pittler.addEventListener("change", openPittler)
+
+
 //NILES//
+
 const niles = document.querySelector("#niles")
 const url19 = "http://127.0.0.1:5500/5-ajuste-medidas-niles.html"
 const url21 = "http://127.0.0.1:5500/5-setup.html"
 const url25 = "http://127.0.0.1:5500/5-alarmes.html"
 
-function openNiles(){
+function openNiles() {
 
     for (i = 0; i < niles.options.length; i++)
-    if (niles.value == "19"){
-        window.location.href = url19
-    }
-    if ( niles.value == "21"){
+        if (niles.value == "19") {
+            window.location.href = url19
+        }
+    if (niles.value == "21") {
         window.location.href = url21
     }
-    if (niles.value == "25"){
+    if (niles.value == "25") {
         window.location.href = url25
     }
 }
@@ -27,15 +44,15 @@ const QG = document.querySelector("#QG")
 const url33 = "http://127.0.0.1:5500/8-Inspecaodepecas.html"
 const url34 = "http://127.0.0.1:5500/8-rugosidade.html"
 
-function openOptions(){
+function openQG() {
 
     for (i = 0; i < QG.options.length; i++)
-    if (QG.value == "33" ){
-        window.location.href = url33
-    }
-    else if (QG.value == "34"){
-        window.location.href = url34
-    }
+        if (QG.value == "33") {
+            window.location.href = url33
+        }
+        else if (QG.value == "34") {
+            window.location.href = url34
+        }
 }
 
-QG.addEventListener("change", openOptions)
+QG.addEventListener("change", openQG)
